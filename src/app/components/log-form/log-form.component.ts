72d33b1bc188c08560a1a885e325bf9dc850e70e
Dan <dan@homegrowndevs.co>
2018-07-01 11:21:20 -0400

    Added form submit, clear form, generateId and delete methods

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -14,16 +14,64 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  // tslint:disable-next-line:no-inferrable-types
+  isNew: boolean = true;
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     // subscribe to the selectedLog observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
+    });
+  }
+
+  onSubmit() {
+    // check for new log
+    if (this.isNew) {
+      // create newish log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      };
+      // add log
+      this.logService.addLog(newLog);
+    } else {
+      // create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // update logssss
+      this.logService.updateLog(updLog);
+    }
+
+    // clear
+    this.clearState();
+  }
+
+  // clears log field, the Add Log and Clear options are tied to 'text'
+  clearState() {
+    this.isNew = true;
+    this.id = '';
+    this.text = '';
+    this.date = '';
+    this.logService.clearState();
+  }
+
+  generateId() {
+    // creates a uuid for onSubmit method - pulled from stackOverflow
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      // tslint:disable-next-line:no-bitwise
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
 
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -13,19 +13,69 @@ export class LogService {
   private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() {
-    this.logs = [
+    /* this.logs = [
       {id: '1', text: 'Generated Components', date: new Date ('06/03/2018 12:12:22')},
       {id: '2', text: 'Added Something', date: new Date ('06/04/2018 8:12:22')},
       {id: '3', text: 'Something Components', date: new Date ('06/22/2018 6:12:22')}
-    ];
+    ]; */
+
+    this.logs = [];
    }
 
    getLogs(): Observable<Log[]> {
-     return of (this.logs);
+     // check if local storage has stuff in it
+     if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+     } else {
+       // takes logs from local storage
+       this.logs = JSON.parse(localStorage.getItem('logs'));
+     }
+     return of (this.logs.sort((a, b) => {
+       return b.date = a.date;
+     }));
    }
 
    setFormLog(log: Log) {
       this.logSource.next(log);
    }
+
+   addLog(log: Log) {
+    this.logs.unshift(log);
+
+    // add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+   }
+
+   // deletes log and adds the updated log
+   updateLog(log: Log) {
+     this.logs.forEach((cur, index) => {
+       if (log.id === cur.id) {
+         this.logs.splice(index, 1);
+       }
+     });
+     this.logs.unshift(log);
+
+     // update locat storage
+     localStorage.setItem('logs', JSON.stringify(this.logs));
+   }
+
+   // just deletes log
+   deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+
+    // delete from local porage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  clearState(log: Log) {
+    this.stateSource.next(true);
+  }
 }
